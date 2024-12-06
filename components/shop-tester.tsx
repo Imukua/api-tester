69de9524137892a51b8cb11f97ed2cd8c10701d3
Ian Mukua <122810834+Imukua@users.noreply.github.com>
2024-12-06 13:02:03 +0300

    Update shop-tester.tsx

--- components/shop-tester.tsx
+++ components/shop-tester.tsx
@@ -66,7 +66,7 @@ export default function ShopTester({ userId, accessToken }: ShopTesterProps) {
     }
   }, [accessToken]);
   useEffect(() => {
-    if (userId && accessToken) {
+    if (accessToken) {
       fetchShops();
     }
   }, [userId, accessToken, fetchShops]);
